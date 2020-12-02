800a910ce6c52b6c323cc5eeedcf4896dd356edf
zdai257 <zdai257@gmail.com>
2020-12-02 17:58:15 +0000

    attempt to inherit to create EKF for >1 RSSIs

--- replay.py
+++ replay.py
@@ -1,6 +1,19 @@
 import sys
 import numpy as np
+from filterpy.kalman import ExtendedKalmanFilter
+from EKF import EKF_Fusion
 
+
+
+class EKF_Fusion2(EKF_Fusion):
+    def __init__(self):
+        super().__init__()
+        self.my_kf = ExtendedKalmanFilter(dim_x=4, dim_z=4)
+    
+
+ekf = EKF_Fusion2()
+print(ekf.my_kf.P)
+print(ekf.visual)
 
 
 # TODO replay the logged LoRa RX messages
