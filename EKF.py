0b6b1ceaf7734687ee158729e98f99277c161b7e
zdai-XPS_GTX1650 <zdai257@gmail.com>
2021-04-29 14:10:24 +0100

    Analysed 'ApartmentInOut3';updated Readme trajectories

--- EKF.py
+++ EKF.py
@@ -23,13 +23,13 @@ import matplotlib
 
 
 # LoRa RX Coordinates in order of Pi-IP: 93, 94, 95, 96, 97
-'''
+
 R1 = np.array([[-2., 10., 0.],
                [12., 10., 0.],
                [13., -1., 0.],
                [5., -1.5, 0.],
                [-5., 4., 0.]])
-'''
+
 # Rotated coordinates for RightHand search
 '''
 R1 = np.array([[10., 2., 0.],
@@ -60,14 +60,14 @@ R1 = np.array([[-2., -3., 0.],
                [8., 22., 0.],
                [2.5, 22, 0.]])
 '''
-
+'''
 # 61 ApartmentInOut3
 R1 = np.array([[-2., -3., 0.],
                [8., -3, 0.],
                [2., 8., 0.],
                [8., 22., 0.],
                [2.5, 22, 0.]])
-
+'''
 # Path Loss Model params
 ALPHA = -55#-45.712  # -28.57 * 1.6
 BETA = -5.06
@@ -432,8 +432,8 @@ class EKF_Fusion():
         self.ax2background = None
         self.cir_lst = []
         self.cir_dict = {0: [], 1: [], 2: [], 3: [], 4: []}
-        #self.clr_lst = ['coral', 'magenta', 'purple', 'brown', 'DeepSkyBlue']  # Color Code to avoid Red/Green Blind
-        self.clr_lst = ['coral', 'magenta', 'gold', 'darkolivegreen', 'limegreen']
+        self.clr_lst = ['coral', 'magenta', 'purple', 'brown', 'DeepSkyBlue']  # Color Code to avoid Red/Green Blind
+        #self.clr_lst = ['coral', 'magenta', 'gold', 'darkolivegreen', 'limegreen']
 
         self.fig2 = plt.figure(figsize=(8, 7))
         gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1])
@@ -760,8 +760,8 @@ class EKF_Fusion():
         #self.handle_arrw_ekf = self.ax21.quiver([self.my_kf.x[0, 0]], [self.my_kf.x[1, 0]], [self.my_kf.x[2, 0]], self.U_ekf, self.V_ekf, self.W_ekf, color='r', length=1., alpha=.7)
 
         # Manually Equal Axis and Limit
-        self.ax21.auto_scale_xyz([-5, 15], [-2, 18], [-1, 3])  # 61Apartment view
-        #self.ax21.auto_scale_xyz([-2.5, 12.5], [-5, 10], [-1, 3])  # Left* search view
+        #self.ax21.auto_scale_xyz([-5, 15], [-2, 18], [-1, 3])  # 61Apartment view
+        self.ax21.auto_scale_xyz([-2.5, 12.5], [-5, 10], [-1, 3])  # Left* search view
         #self.ax21.auto_scale_xyz([-2.5, 12.5], [-12.5, 2.5], [-1, 3])  # RightVicon2 view
         #self.ax21.auto_scale_xyz([-5, 15], [-15, 5], [-1, 3])  # OneAnchorTest view
 
@@ -889,7 +889,7 @@ class EKF_Fusion():
             RotEular = quat2euler(q)
 
             t0 = datetime.utcfromtimestamp(Times[i, 0] + Times[i, 1] / (10 ** 9))
-            abs_x = Trans[i, 0] - 3  # -3 to patch ApartmentInOut3
+            abs_x = Trans[i, 0]  # -3 to patch ApartmentInOut3
             abs_y = Trans[i, 1]
             abs_yaw = RotEular[2]
 
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Replay data reception and Visualise path in a pack of 10 Odometry outputs by
 
 ```
-python3 rx_lora.py LST 10
+./rx_lora.py LST 10
 ```
 
 We investigated localisation performance in the Oxford building scenario as below. Red triangles are deployed LoRa RX nodes. We mainly traversed the square-shaped corridors and the middle room 209. LiDAR provided cm-magnitude of accuracy which is regarded as ground-truth path marked in grey. Extended Kalman Filter (EKF) and Particle Filters (PF) are utilized to conduct location fusion.
@@ -16,20 +16,20 @@ Three-round right-hand search:
 
 ![left3](https://github.com/zdai257/LoRaRX/blob/main/Trajectory/demo0324Left3.png)
 
-Two-round right-hand search entering Vicon room:
+Two-round right-hand search entering Vicon room in the 1st lap:
 
 ![leftvicon2](https://github.com/zdai257/LoRaRX/blob/main/Trajectory/demo0324LeftVicon2.png)
-
-Two-round left-hand search entering Vicon room:
-
-![rightvicon2](https://github.com/zdai257/LoRaRX/blob/main/Trajectory/demo0324RightVicon2.png)
 
 Two-round right-hand search entering Vicon room in the 2nd lap:
 
 ![rightviconlast](https://github.com/zdai257/LoRaRX/blob/main/Trajectory/demo0421RightViconLast.png)
 
+Ground-floor apartment search interior and outside:
+
+![ApartmentInOut3](https://github.com/zdai257/LoRaRX/blob/main/Trajectory/ApartmentInOut3.png)
+
 ### SIR Particle Filter
 
-Two-round right-hand search using Particle Filter at 1Hz real-time refresh rate:
+Two-round right-hand search using Particle Filter at 1Hz refresh rate:
 
 ![pf_left2](https://github.com/zdai257/LoRaRX/blob/main/Trajectory/demo0.png)
